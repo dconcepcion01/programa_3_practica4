06bf620c3fa69639bf7d9cac338a6ff58c3a6958
Danny <dannyconcepcion01@gmail.com>
2020-09-20 20:17:36 -0400

    Agregar archivos de proyecto.

---
+++ programa_3_practica4/Aritmetica.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_3_practica4
+
+{   //clase Aritmetica
+    class Aritmetica
+    {
+        //Metodo operacion suma
+        public static int sumar(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+
+        }
+        //Metodo operacion multiplicacion
+        public static int Multi(int numero1, int numero2)
+        {
+            return numero1 * numero2;
+
+        }
+        //Metodo operacion resta
+        public static int resta(int numero1, int numero2)
+        {
+            return numero1 - numero2;
+        }
+        //Metodo operacion division
+        public static int division(int numero1, int numero2)
+        {
+            return numero1 / numero2;
+
+        }
+    }
+}
---
+++ programa_3_practica4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_3_practica4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("ESTE PROGRAMA TE CALCULA TE REALIZA LAS OPERACIONES DE CALCULOS");
+            Aritmetica.sumar(3, 4);
+
+            Console.SetCursorPosition(5, 7);
+            Console.WriteLine("El resultado de la suma es: " + Aritmetica.sumar(4, 5));
+
+
+            Console.ReadKey();
+
+            Aritmetica.Multi(3, 4);
+
+            Console.SetCursorPosition(5, 8);
+            Console.WriteLine("El resultado de la Multiplicacion es: " + Aritmetica.Multi(4, 5));
+
+
+            Console.ReadKey();
+
+            Aritmetica.resta(3, 4);
+
+            Console.SetCursorPosition(5, 9);
+            Console.WriteLine("El resultado de la Resta es: " + Aritmetica.resta(4, 5));
+
+
+            Console.ReadKey();
+
+            Aritmetica.division(3, 4);
+
+            Console.SetCursorPosition(5, 10);
+            Console.WriteLine("El resultado de la Division es: " + Aritmetica.division(4, 5));
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
